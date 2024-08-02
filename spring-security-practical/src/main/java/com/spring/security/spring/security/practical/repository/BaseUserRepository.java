7ec577d9fde3c4b2f8068b484956d16d6acd7e97
kapilan1 <kapilan@codelantic.com>
2024-08-02 22:12:59 +0530

    repository created

--- spring-security-practical/src/main/java/com/spring/security/spring/security/practical/repository/BaseUserRepository.java
+++ spring-security-practical/src/main/java/com/spring/security/spring/security/practical/repository/BaseUserRepository.java
@@ -4,4 +4,5 @@ import com.spring.security.spring.security.practical.entity.BaseUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BaseUserRepository extends JpaRepository<BaseUser,Long> {
+
 }
