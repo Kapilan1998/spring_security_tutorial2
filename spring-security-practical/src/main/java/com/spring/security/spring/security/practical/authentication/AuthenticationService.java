793eef7bc17db8e10c56327d3a83dfcf4f7b26b9
kapilan1 <kapilan@codelantic.com>
2024-08-11 23:31:07 +0530

    secret key added + Jwts added

--- spring-security-practical/src/main/java/com/spring/security/spring/security/practical/authentication/AuthenticationService.java
+++ spring-security-practical/src/main/java/com/spring/security/spring/security/practical/authentication/AuthenticationService.java
@@ -1,17 +1,25 @@
 package com.spring.security.spring.security.practical.authentication;
 
+import com.spring.security.spring.security.practical.config.JwtService;
 import com.spring.security.spring.security.practical.entity.BaseUser;
 import com.spring.security.spring.security.practical.repository.BaseUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
 public class AuthenticationService {
 
     @Autowired
     private AuthenticationManager authenticationManager;
     @Autowired
     BaseUserRepository baseUserRepository;
+    @Autowired
+    JwtService jwtService;
 
     public AuthenticationResponse login(AuthenticationRequest authenticationRequest) {
         UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
@@ -22,5 +30,13 @@ public class AuthenticationService {
 
         String jwt = jwtService.generateToken(baseUser,generateExtraClaims(baseUser));
         return new AuthenticationResponse(jwt);
+    }
+
+    private Map<String,Object> generateExtraClaims(BaseUser baseUser) {
+        Map<String,Object> extraClaims = new HashMap<>();
+        extraClaims.put("name",baseUser.getName());
+        extraClaims.put("role",baseUser.getRole().name());
+
+        return extraClaims;
     }
 }
